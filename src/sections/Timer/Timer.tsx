969ebb037e630cd0307bb7e6b1c1ccfa2762dcde
DinoPimenteiro <gabrielwilliam234@gmail.com>
2026-04-25 21:30:14 -0300

    update timer logic

--- src/sections/Timer/Timer.tsx
+++ src/sections/Timer/Timer.tsx
@@ -2,30 +2,95 @@ import styles from "./Timer.module.css";
 import Input from "../../components/InputComponent/Input";
 import Button from "../../components/Button/Button";
 import Cycles from "../../components/Cycles/Cycles";
-import { Play } from "lucide-react";
-import useTaskContext from "../../hooks/useTaskContext";
+import { Play, StopCircle } from "lucide-react";
+import useTaskContext from "../../contexts/TaskContext";
+import { useState } from "react";
+import type { TaskModelType } from "../../types/Task/TaskModelType";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { TypeAction } from "../../contexts/TaskContext/taskActions";
+import { toastifyAdapter } from "../../adapters/toastifyAdapter";
 
 const Timer = () => {
-  const taskContextValue = useTaskContext();
+  const [taskName, setTaskName] = useState("");
 
-  console.log(taskContextValue);
+  const { state, dispatch } = useTaskContext();
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
+  const handleCreateTask = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    toastifyAdapter.dismiss();
+
+    const taskName = event.currentTarget.task.value.trim();
+    if (taskName.length === 0) {
+      toastifyAdapter.warn("Digite uma tarefa!");
+      return;
+    }
+
+    const newTask: TaskModelType = {
+      id: Date.now().toString(),
+      name: taskName,
+      duration: state.config[nextCycleType],
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      type: nextCycleType,
+    };
+
+    dispatch({ type: TypeAction.START_TASK, payload: newTask });
+    toastifyAdapter.success("Tarefa iniciada com sucesso!");
+  };
+
+  function handleInterruptTask() {
+    toastifyAdapter.dismiss();
+    toastifyAdapter.info("Tarefa interrompida.");
+    dispatch({ type: TypeAction.INTERRUPT_TASK });
+  }
+
+  const tipsForEachInterval = {
+    workTime: (
+      <span>
+        {" "}
+        Se mantenha focado por <b>{state.config.workTime}min</b>
+      </span>
+    ),
+    shortBreakTime: (
+      <span>
+        Aproveite o descanso de <b>{state.config.shortBreakTime}min</b>
+      </span>
+    ),
+    longBreakTime: <span>Hora do descanso longo, aproveite!</span>,
+  };
+
   return (
     <>
-      <span className={styles.time}>00:00</span>
+      <span className={styles.time}>{state.formattedSecondsRemaining}</span>
 
-      <div className={styles.taskContainer}>
-        <p>task:</p>
-        <Input placeholder="Digite sua tarefa" type="text" />
-        <p>
-          Nesse ciclo <b>foque</b> por <b>25 min.</b>
-        </p>
+      <form onSubmit={handleCreateTask} className={styles.taskContainer}>
+        <label id="task">task:</label>
+        <Input
+          disabled={!!state.activeTask}
+          id="task"
+          placeholder="Digite sua tarefa"
+          type="text"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+        <p>{state.activeTask && tipsForEachInterval[state.activeTask.type]}</p>
 
-        <Cycles />
-        <Button>
-          {" "}
-          <Play /> Iniciar
+        {state.currentCycle > 0 && <Cycles />}
+        <Button
+          type={state.activeTask ? "button" : "submit"}
+          color={state.activeTask ? "red" : "green"}
+          title={state.activeTask ? "Parar tarefa" : "Iniciar tarefa"}
+          aria-label={state.activeTask ? "Parar tarefa" : "Iniciar tarefa"}
+          onClick={state.activeTask ? handleInterruptTask : () => {}}
+          key={state.activeTask ? "stop" : "play"}
+        >
+          {state.activeTask ? <StopCircle /> : <Play />}
         </Button>
-      </div>
+      </form>
     </>
   );
 };
